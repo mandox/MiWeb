beb409e55f17fae58f4c550d23a14569b96f06a7
Armando Gonzales Cajes <gonzalesarmando36@gmail.com>
2012-07-07 09:58:25 -0500

    Se Agrego Tipo

---
+++ MiWeb/src/java/com/miweb/com/Tipo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miweb.com;
+
+/**
+ *
+ * @author PC
+ */
+public class Tipo {
+    String t1;
+    String t2;
+
+    public Tipo() {
+    }
+
+    public String getT1() {
+        return t1;
+    }
+
+    public void setT1(String t1) {
+        this.t1 = t1;
+    }
+
+    public String getT2() {
+        return t2;
+    }
+
+    public void setT2(String t2) {
+        this.t2 = t2;
+    }
+    
+}
